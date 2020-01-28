908d7a393421eab268dbe87283a495dbb67dbe09
liuyi12138 <852833062@qq.com>
2020-01-28 14:44:46 +0800

    2020/1/28

---
+++ 1127.c
@@ -0,0 +1,12 @@
+int tribonacci(int n){
+    if(n == 0) return 0;
+    if(n == 1 || n == 2) return 1;
+    int result[n+1];
+    result[0] = 0;
+    result[1] = 1;
+    result[2] = 1;
+    for(int i=3; i<=n; ++i){
+        result[i] = result[i-1] + result[i-2] + result[i-3];
+    }
+    return result[n];
+}
