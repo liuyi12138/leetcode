b67ab9818fddc1ec1f248089d875c05f133358f7
liuyi12138 <852833062@qq.com>
2020-05-08 18:33:17 +0800

    2020/5/8

---
+++ questionsByNum/221.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int maximalSquare(vector<vector<char>>& matrix) {
+        if(matrix.size() == 0 || matrix[0].size() == 0) return 0;
+        vector<vector<int>> dp(matrix.size(), vector<int> temp(matrix[0].size()));
+        for(int i = 0; i < matrix.size(); ++i){
+            for(int j = 0; j < matrix[i].size(); ++j){
+
+            }
+        }
+        return 0;
+    }
+};
